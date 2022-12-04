65fae72a8c1dd0766b46d46b871c01b638197c59
Irena <irena.vangelova10@gmail.com>
2022-12-04 12:37:46 +0100

    init

--- client/src/pages/Quotes.js
+++ client/src/pages/Quotes.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import { Button } from "@mui/material";
+import {CircularProgress} from "@mui/material";
+import Box from '@mui/material/Box';
 
 const columns = [
   { field: '_id', headerName: 'ID', width: 150 },
@@ -11,36 +13,85 @@ const columns = [
 
 const Quotes = () => {
 
-    const [quotesData, setQuoteData] = useState([]);
+    const [quotesData, setQuoteData] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
       fetch("https://api.quotable.io/quotes")
       .then((data) => data.json())
-        .then((data) => setQuoteData(data.results));
+        .then((data) => {
+          let tableRows = [];
+          data.results.forEach((element) => {
+            let row = {
+              _id: element._id,
+              author: element.author,
+              content: element.content,
+              nationality: ""
+            }
+            tableRows.push(row);
+          });
+          setQuoteData(tableRows);
+          setIsLoaded(true)
+        });
       },[])
 
-    console.log(quotesData);
-
-    const getNationality = (name) => {
+    const getNationality = async (name) => {
+     
       let nationality = null;
 
-      fetch("https://api.nationalize.io/?name=" + name, {method: 'GET', mode: "no-cors", headers: {
+      await fetch("https://api.nationalize.io/?name=" + name, {method: 'GET', mode: "cors", headers: {
         'Content-Type': 'application/json'
-      } })
-      .then((data) => console.log(data))
+      }})
+      .then((data) => {return data.json()})
+      .then((data) => nationality = data.country[0].country_id);
+    
+      return nationality;
     }
 
-    getNationality("Michael");
+    const populateNationality = async () => {
+    if (quotesData != null) {
+      let newData = quotesData;
+      newData.forEach(async (element) => {
+        let nationality = await getNationality(element.author.split(" ")[0]);
+        element.nationality = nationality;
+      });
+
+      setQuoteData(newData);
+      setIsLoaded(false);
+    }
+  }
+
+  useEffect(() => {
+    if(isLoaded) {
+      populateNationality();
+    }
+  }, [isLoaded])
+
+  // useEffect(() => {
+  //   console.log(quotesData);
+  // },[quotesData])
+
+
+  let table = quotesData != null ? (<div style={{ height: 630, width: '100%' }}><DataGrid
+    getRowId={(row) => row._id}
+    rows={quotesData}
+    columns={columns}
+    pageSize={10}
+  />
+    <Button variant="contained" href="http://localhost:3000/random-quote">Random Quote</Button></div>) : (<Box sx={{
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      position: 'absolute',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}><CircularProgress /></Box>);
     
   return (
     <div style={{ height: 630, width: '100%' }}>
-    <DataGrid
-      getRowId={(row) => row._id}
-      rows={quotesData}
-      columns={columns}
-      pageSize={10}
-    />
-    <Button variant="contained" href="http://localhost:3000/random-quote">Random Quote</Button>
+        {table}
     </div>
   );
 
